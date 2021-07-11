5c03c8e0068a601b76a26a0b8eb649600c83ccab
BaekYeonSun <55418359+BaekYeonSun@users.noreply.github.com>
2021-07-12 03:18:40 +0900

    5주차과제(CRUD)

--- 5주차과제/router.js
+++ 5주차과제/router.js
@@ -7,18 +7,49 @@ module.exports = function(app, Board){
         board.save(function(err){
             if(err){
                 console.error(err);
-                res.json({message : 'fail'});
+                res.json({ message : 'fail' });
                 return;
             }
-            res.json({message : 'success'});
+            res.json({ message : 'success' });
         });
     });
 
-    app.get('/board/:title', function(req, res){
-        Board.findOne({title: req.params.title}, function(err, board){
-            if(err) return res.status(500).json({error: err});
-            if(!board) return res.status(404).json({error: '해당 제목의 게시글이 존재하지 않습니다.'});
-            res.json(board);
-        })
-    });
+    // app.get('/board', function (req, res){
+    //     Board.find(function(err, board){
+    //         if(err) return res.status(500).json({ error: err });
+    //         if(!board) return res.status(404).json({ error: '해당 제목의 게시글이 존재하지 않습니다.' });
+    //         res.json(board);
+    //     });
+    // })
+    //
+    // app.get('/board/:title', function(req, res){
+    //     Board.findOne({ title: req.params.title }, function(err, board){
+    //         if(err) return res.status(500).json({ error: err });
+    //         if(!board) return res.status(404).json({ error: '해당 제목의 게시글이 존재하지 않습니다.' });
+    //         res.json(board);
+    //     })
+    // });
+    //
+    // app.put('/board/:title', function(req, res){
+    //     Board.findOne({ title: req.params.title }, function(err, board){
+    //         if(err) return res.status(500).json({ error: err });
+    //         if(!board) return res.status(404).json({ error: '해당 제목의 게시글이 존재하지 않습니다.' });
+    //
+    //         board.body = req.body.body;
+    //         board.save(function(err){
+    //             if(err) res.status(500).json({ error: err });
+    //             res.json({ message: 'update' });
+    //         });
+    //     });
+    // });
+    //
+    // app.delete('/board/:title', function(req, res){
+    //     Board.remove({ title: req.params.title }, function(err, output){
+    //         if(err) return res.status(500).json({ error: err });
+    //
+    //         res.json({ message: 'delete' });
+    //
+    //         res.status(204).end();
+    //     })
+    // });
 }
